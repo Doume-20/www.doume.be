4ab56cbcc49d6759ac0dda7c25edfdf630feda9a
Doume20 <153982012+Doume-20@users.noreply.github.com>
2026-09-10 14:22:46 +0200

    Doing some stuff to ogImage

--- app/pages/github.vue
+++ app/pages/github.vue
@@ -4,6 +4,7 @@ import GithubSection from "@/components/sections/GithubSection.vue"
 useSeoMeta({
   title: "Github",
   description: "infos sur mon Github",
+  ogImage: "../favicon.ico",
   ogTitle: "Github",
   ogDescription: "'jour",
 })
